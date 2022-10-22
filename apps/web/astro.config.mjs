44c53ee23eabfa8a7066616b8d9cd76ee36b7f62
Limerio <44976231+Limerio@users.noreply.github.com>
2022-10-22 19:00:00 +0200

    Pin dependencies & Update astro.config.mjs

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import node from '@astrojs/node'
 
 export default defineConfig({
   outDir: '../../dist/apps/web',
+  output: 'server',
+  adapter: node({
+    mode: 'middleware',
+  }),
   integrations: [react()],
-});
+})
